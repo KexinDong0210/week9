d4d1b9f0057147d65df9427a7b2902007cc7db1b
KexinDong0210 <107507567+KexinDong0210@users.noreply.github.com>
2022-07-12 16:38:02 -0400

    Update sketch.js

--- week9/07_twoParameters/sketch.js
+++ week9/07_twoParameters/sketch.js
@@ -1,7 +1,7 @@
 let dataServer;
-let pubKey = "YOUR PUBLISH KEY HERE";
-let subKey = "YOUR SUBSCRIBE KEY HERE";
-let secretKey = "YOUR SECRET KEY HERE";
+let pubKey = "pub-c-8023b38b-b78d-4dc5-80cb-ea56808e1983";
+let subKey = "sub-c-1c02ba5e-e002-4851-9f24-cb7062034440";
+let secretKey = "sec-c-Y2FjZjRmMTktMzdmYS00ZjY5LTk1Y2EtMWJhYzI3MmY3YjEz";
 
 let channelName = "NFCs"; // we don't define this right away!
 
